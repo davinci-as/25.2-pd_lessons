51aaca4c99fc320f8d8d3b8ab046c0e2c3b02929
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-09-30 19:16:47 -0300

    feat: add tag prop

--- vite_template/src/App.jsx
+++ vite_template/src/App.jsx
@@ -6,20 +6,36 @@ function App() {
 
   const cards = [
     {
+      tag: "content-01",
       title: "prueba de titulo 1",
       buttonText: "Call To Action 1",
       className: age > 18 ? "green" : "red",
     },
     {
+      tag: "content-02",
+      title: "prueba de titulo 2",
+      buttonText: "Call To Action 2",
+      className: age <= 18 ? "green" : "red",
+    },
+    {
+      tag: "content-01",
       title: "prueba de titulo 2",
       buttonText: "Call To Action 2",
       className: age <= 18 ? "green" : "red",
     },
   ];
 
+  const tempCards = [];
+
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].tag === "content-01") {
+      tempCards.push(cards[i]);
+    }
+  }
+
   return (
     <>
-      {cards.map((card) => (
+      {tempCards.map((card) => (
         <Card
           title={card.title}
           buttonText={card.buttonText}
