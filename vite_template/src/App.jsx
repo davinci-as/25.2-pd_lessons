7dd6e83c44c69693446a6f9e8a94468b325b24db
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-09-30 20:00:46 -0300

    feat: render list on click

--- vite_template/src/App.jsx
+++ vite_template/src/App.jsx
@@ -1,38 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import Button from "./components/Button";
+
+const age = 26;
+const originalCards = [
+  {
+    tag: "content-01",
+    title: "prueba de titulo 1",
+    buttonText: "Call To Action 1",
+    className: age > 18 ? "green" : "red",
+  },
+  {
+    tag: "content-02",
+    title: "prueba de titulo 2",
+    buttonText: "Call To Action 2",
+    className: age <= 18 ? "green" : "red",
+  },
+  {
+    tag: "content-01",
+    title: "prueba de titulo 2",
+    buttonText: "Call To Action 2",
+    className: age <= 18 ? "green" : "red",
+  },
+];
+
+const criteria = (card) => {
+  if (card.tag === "content-02") {
+    return false;
+  }
+  return true;
+};
 
 function App() {
-  const age = 26;
-
-  const criteria = (card) => {
-    if (card.tag === "content-02") {
-      return false;
-    }
-    return true;
-  };
-  const cards = [
-    {
-      tag: "content-01",
-      title: "prueba de titulo 1",
-      buttonText: "Call To Action 1",
-      className: age > 18 ? "green" : "red",
-    },
-    {
-      tag: "content-02",
-      title: "prueba de titulo 2",
-      buttonText: "Call To Action 2",
-      className: age <= 18 ? "green" : "red",
-    },
-    {
-      tag: "content-01",
-      title: "prueba de titulo 2",
-      buttonText: "Call To Action 2",
-      className: age <= 18 ? "green" : "red",
-    },
-  ];
+  const [cards, setCards] = useState([]);
 
   return (
     <>
+      <Button
+        onClick={() => {
+          setCards(originalCards);
+        }}
+      />
       {cards.filter(criteria).map((card, i) => (
         <Card
           key={i}
