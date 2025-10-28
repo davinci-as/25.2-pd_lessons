677da9b50380564345422fef9a4200c67cfb83e1
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-28 20:36:58 -0300

    feat: fetch data from endpoint
    endpoint --> /api/cards

--- vite_template/src/lib/data.js
+++ vite_template/src/lib/data.js
@@ -1,29 +1,11 @@
-const age = 26;
-
 export const getData = async () => {
-  return new Promise((done) => {
-    setTimeout(async () => {
-      const originalCards = [
-        {
-          tag: "content-01",
-          title: "prueba de titulo 1",
-          buttonText: "Call To Action 1",
-          className: age > 18 ? "green" : "red",
-        },
-        {
-          tag: "content-02",
-          title: "prueba de titulo 2",
-          buttonText: "Call To Action 2",
-          className: age <= 18 ? "green" : "red",
-        },
-        {
-          tag: "content-01",
-          title: "prueba de titulo 2",
-          buttonText: "Call To Action 2",
-          className: age <= 18 ? "green" : "red",
-        },
-      ];
-      done(originalCards);
-    }, 4000);
-  });
+  try {
+    const raw = await fetch("/api/cards")
+    const data = await raw.json();
+    if (!data) return [];
+    return data.data;
+  } catch {
+    return [];
+  }
+
 };
