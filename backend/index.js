03165a4bc9bfc2b45a642e9b2a69ee7224732ab4
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-28 20:11:10 -0300

    feat: add cards endpoint

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,34 @@ app.get('/api', (req, res) => {
   res.json(resto);
 });
 
+app.get('/api/cards/', (req, res) => {
+  const age = 26;
+
+  const originalCards = [
+    {
+      tag: "content-01",
+      title: "prueba de titulo 1",
+      buttonText: "Call To Action 1",
+      className: age > 18 ? "green" : "red",
+    },
+    {
+      tag: "content-02",
+      title: "prueba de titulo 2",
+      buttonText: "Call To Action 2",
+      className: age <= 18 ? "green" : "red",
+    },
+    {
+      tag: "content-01",
+      title: "prueba de titulo 2",
+      buttonText: "Call To Action 2",
+      className: age <= 18 ? "green" : "red",
+    }
+  ]
+  res.json({
+    data: originalCards
+  })
+});
+
 app.get('/api/version', (req, res) => {
   res.json(objectoRetornado.version);
 });
