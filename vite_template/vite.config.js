766efc78499670a3ad96ae95ab6fa4d61ccf0c56
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-28 20:36:29 -0300

    chore: setup api proxy

--- vite_template/vite.config.js
+++ vite_template/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/api': "http://localhost:3000/api"
+  },
+  server: {
+    cors: true,
+    proxy: {
+      '/api': "http://localhost:3000",
+    }
+  }
 })
