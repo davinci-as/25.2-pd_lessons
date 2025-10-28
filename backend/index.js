8e0efbbd80aa2cfff5f7112483156a089c5d9e90
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-28 19:47:06 -0300

    feat: add static root path

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
+const e = require('express');
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
+const ROOT_FOLDER = 'dist';
 
 const banner = `
   <div class="banner">
     <h1>Ejemplo de banner inyectado</h1>
   </div>
   `
+
+app.use(express.static(
+  path.join(__dirname, ROOT_FOLDER)
+));
 
 app.get('/admin', (req, res) => {
   res.send(`
