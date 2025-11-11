41f8b0d4e44a4e24bbcbf7c9e644931d732d6b64
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-11-11 19:59:39 -0300

    fix: update card list endpoint

--- vite_template/src/lib/data.js
+++ vite_template/src/lib/data.js
@@ -1,11 +1,10 @@
 export const getData = async () => {
   try {
-    const raw = await fetch("/api/cards")
+    const raw = await fetch("/api/cards/list");
     const data = await raw.json();
     if (!data) return [];
     return data.data;
   } catch {
     return [];
   }
-
 };
