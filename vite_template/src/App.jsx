6a8e142307eba6444b22c4cc539d0b65d5c0ab3c
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-11-11 20:32:36 -0300

    feat: add new card page

--- vite_template/src/App.jsx
+++ vite_template/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { HomePage } from "./pages/Home";
+import { NewCard } from "./pages/NewCard";
 function App() {
   return <HomePage />;
 }
---
+++ vite_template/src/pages/NewCard.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export const NewCard = () => {
+  const [title, setTitle] = useState("");
+  const [tag, setTag] = useState("");
+  const [buttonText, setButtontext] = useState("");
+
+  const submitCard = (e) => {
+    e.preventDefault();
+    const data = {
+      title,
+      tag,
+      buttonText,
+    };
+
+    fetch("/api/cards/add/", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+  };
+  return (
+    <>
+      <h1>New card</h1>
+      <form
+        onSubmit={submitCard}
+        style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Ingrese el titulo de la tarjeta"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="tag"
+          placeholder="Ingrese la etiqueta de la tarjeta"
+          onChange={(e) => setTag(e.target.value)}
+        />
+        <input
+          type="text"
+          name="buttonText"
+          placeholder="Ingrese el texto del botón"
+          onChange={(e) => setButtontext(e.target.value)}
+        />
+
+        <input type="submit" value={"enviar"} />
+      </form>
+    </>
+  );
+};
