edc82ae5ab14ebef1e1bb33e17fb42b4f4a12d27
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-11-18 20:19:27 -0300

    feat: add login page structure

---
+++ vite_template/src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYx8s2YexgR5daMt50-N6GU8Q7gfUGV00",
+  authDomain: "plataformas-252.firebaseapp.com",
+  projectId: "plataformas-252",
+  storageBucket: "plataformas-252.firebasestorage.app",
+  messagingSenderId: "733729672473",
+  appId: "1:733729672473:web:f9cb24c22925e57b38c988",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- vite_template/src/pages/Login.jsx
+++ vite_template/src/pages/Login.jsx
@@ -1,6 +1,10 @@
 import styles from "./login.module.css";
+import { authWithEmail } from "../lib/auth";
+import { useState } from "react";
 
 export const LoginPage = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+
   const validateData = ({ email, password }) => {
     if (email == "" || email == undefined) return false;
     if (password == "" || password == undefined) return false;
@@ -15,18 +19,44 @@ export const LoginPage = () => {
     };
     if (!validateData(data)) return;
 
-    fetch("/api/users/add/", {
+    authWithEmail(data)
+      .then((data) => {
+        if (!data?.user?.uid) return;
+        const uid = data?.user?.uid;
+
+        fetch("/api/users/find/", {
+          method: "POST",
+          body: JSON.stringify({ uid }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => alert(data.message));
+      })
+      .catch((error) => {
+        if (error.code == "auth/invalid-credential") {
+          setErrorMessage("Credenciales inválidas");
+        } else {
+          setErrorMessage("No se pudo iniciar sesión");
+        }
+      });
+
+    /*fetch("/api/users/add/", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
-    });
+    });*/
 
     //fetch a /users/add
   };
   return (
     <div className={styles.container}>
+      {errorMessage && (
+        <div className={styles.errorMessage}>{errorMessage}</div>
+      )}
       <h1>Login</h1>
       <form className={styles.formContainer} onSubmit={onSubmit}>
         <input type="email" name="email" />
