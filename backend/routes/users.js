c73d6bd89363751d6e0d766534f9c569c2236d5c
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-11-18 20:18:40 -0300

    feat: add find method on users endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,6 +6,13 @@ route.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
 
+route.post("/find", (req, res) => {
+  const uid = req.body?.uid;
+
+  console.log("uid", uid);
+  res.json({ message: "ok" });
+});
+
 route.post("/add", (req, res) => {
   const body = req.body;
   console.log("access", body);
